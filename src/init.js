cedf7ee2e5fd90b33f71a1e91ca33561de8c5d32
Steven Berry <sberry.me>
2013-09-25 23:48:44 -0400

    Removing obsolete code.

--- src/init.js
+++ src/init.js
@@ -14,12 +14,6 @@ function init(selector, context) {
 	else if (typeof selector === 'string') {
 		if (selector.charAt(0) === '<' && selector.charAt(selector.length - 1) === '>') {
 			return parseHTML(selector);
-			/*
-			if (match = re_tagName.exec(selector)) {
-				dummy.innerHTML = selector;
-				return arrx(dummy.children[0].cloneNode(true));
-			}
-			*/
 		}
 		
 		this.selector = selector;
